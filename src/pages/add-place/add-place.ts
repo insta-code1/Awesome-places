33bc7291381d39e224abf032ca4c5f831b4a6045
insta-code1 <eoinkane1@gmail.com>
2017-05-11 14:35:58 +0100

    Setting google map config when opening set-location page

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 import { ModalController } from 'ionic-angular';
 import { NgForm } from "@angular/forms";
 
+import { Location } from './../../models/location';
+
 
 @Component({
   selector: 'page-add-place',
   templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
+  location: Location = {
+    lat: 40.7624324,
+    lng: -73.9759827
+  };
 
   constructor(private modalCtrl: ModalController) {
   }
@@ -18,7 +24,7 @@ export class AddPlacePage {
   }
 
   onOpenMap() {
-    const modal = this.modalCtrl.create(SetLocationPage);
+    const modal = this.modalCtrl.create(SetLocationPage, { location: this.location });
     modal.present();
   }
 
--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,24 +1,16 @@
+import { NavParams } from 'ionic-angular';
+import { Location } from './../../models/location';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SetLocation page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+
 @Component({
   selector: 'page-set-location',
   templateUrl: 'set-location.html',
 })
 export class SetLocationPage {
+  location: Location;  
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams: NavParams) {
+    this.location = this.navParams.get('location');
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SetLocation');
-  }
-
 }
